c24252ce32f5b935e3c401693077953043853daf
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2019-04-27 18:20:43 -0300

    updated package features

---
+++ app/src/main/java/cesar1287/com/github/desafiopicpay/features/creditCard/CreditCardActivity.kt
@@ -0,0 +1,13 @@
+package cesar1287.com.github.desafiopicpay.features.creditCard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cesar1287.com.github.desafiopicpay.R
+
+class CreditCardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_credit_card)
+    }
+}
---
+++ app/src/main/java/cesar1287/com/github/desafiopicpay/features/home/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package cesar1287.com.github.desafiopicpay.features.home.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cesar1287.com.github.desafiopicpay.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
