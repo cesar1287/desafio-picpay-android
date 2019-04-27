75bf2195c7dccbb1ced9acaf30cc5410871fbc41
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2019-04-27 13:26:50 -0300

    Initial commit

---
+++ app/src/main/java/cesar1287/com/github/desafiopicpay/MainActivity.kt
@@ -0,0 +1,12 @@
+package cesar1287.com.github.desafiopicpay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
