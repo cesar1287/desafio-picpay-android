71b604e0e344f50823bf8db212b9e41072acfca2
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2019-04-27 20:35:10 -0300

    finished home screen

--- app/src/main/java/cesar1287/com/github/desafiopicpay/features/home/activity/MainActivity.kt
+++ app/src/main/java/cesar1287/com/github/desafiopicpay/features/home/activity/MainActivity.kt
@@ -2,12 +2,40 @@ package cesar1287.com.github.desafiopicpay.features.home.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import cesar1287.com.github.desafiopicpay.R
+import cesar1287.com.github.desafiopicpay.core.model.User
+import cesar1287.com.github.desafiopicpay.features.home.adapter.HomeAdapter
+import cesar1287.com.github.desafiopicpay.features.home.viewmodel.MainViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel by lazy { MainViewModel() }
+    private lateinit var homeAdapter: HomeAdapter
+    private val usersList: MutableList<User> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupRecyclerView()
+
+        mainViewModel.fetchUsers()
+
+        mainViewModel.usersLiveData.observe(this, Observer {
+            pbMainLoading.visibility = View.GONE
+            usersList.addAll(it)
+            homeAdapter.notifyDataSetChanged()
+        })
+    }
+
+    private fun setupRecyclerView() {
+        val layoutManager = LinearLayoutManager(this@MainActivity)
+        rvMainList.layoutManager = layoutManager
+        homeAdapter = HomeAdapter(this@MainActivity, usersList)
+        rvMainList.adapter = homeAdapter
     }
 }
--- app/src/main/java/cesar1287/com/github/desafiopicpay/features/home/viewmodel/MainViewModel.kt
+++ app/src/main/java/cesar1287/com/github/desafiopicpay/features/home/viewmodel/MainViewModel.kt
@@ -1,2 +1,35 @@
 package cesar1287.com.github.desafiopicpay.features.home.viewmodel
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import cesar1287.com.github.desafiopicpay.core.api.ApiService
+import cesar1287.com.github.desafiopicpay.core.api.repository.home.UserRepository
+import cesar1287.com.github.desafiopicpay.core.model.User
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
+
+class MainViewModel : ViewModel(){
+
+    private val parentJob = SupervisorJob()
+
+    private val coroutineContext: CoroutineContext
+        get() = parentJob + Dispatchers.Default
+
+    private val scope = CoroutineScope(coroutineContext)
+
+    private val repository : UserRepository = UserRepository(ApiService.picpayApi)
+
+    val usersLiveData = MutableLiveData<MutableList<User>>()
+
+    fun fetchUsers(){
+        scope.launch {
+            val users = repository.getUsers()
+            usersLiveData.postValue(users)
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        parentJob.cancel()
+    }
+}
