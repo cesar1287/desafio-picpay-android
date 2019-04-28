9857cf4a41957432f03f05a73592828fd44611b8
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2019-04-28 15:31:48 -0300

    finished cover screen to credit card

--- app/src/main/java/cesar1287/com/github/desafiopicpay/features/creditCard/activity/CreditCardCoverActivity.kt
+++ app/src/main/java/cesar1287/com/github/desafiopicpay/features/creditCard/activity/CreditCardCoverActivity.kt
@@ -1,13 +1,13 @@
-package cesar1287.com.github.desafiopicpay.features.creditCard
+package cesar1287.com.github.desafiopicpay.features.creditCard.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import cesar1287.com.github.desafiopicpay.R
 
-class CreditCardActivity : AppCompatActivity() {
+class CreditCardCoverActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_credit_card)
+        setContentView(R.layout.activity_credit_card_cover)
     }
 }
