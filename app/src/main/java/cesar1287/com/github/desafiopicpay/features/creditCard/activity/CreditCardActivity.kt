538fdd392760c2564c0f1b37396ac660a7316632
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2019-04-28 22:32:23 -0300

    finished credit card screen with feature save

--- app/src/main/java/cesar1287/com/github/desafiopicpay/features/creditCard/activity/CreditCardActivity.kt
+++ app/src/main/java/cesar1287/com/github/desafiopicpay/features/creditCard/activity/CreditCardActivity.kt
@@ -2,17 +2,45 @@ package cesar1287.com.github.desafiopicpay.features.creditCard.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import cesar1287.com.github.desafiopicpay.R
+import cesar1287.com.github.desafiopicpay.core.util.CreditCard.KEY_HASH_CARD_NUMBER
+import cesar1287.com.github.desafiopicpay.core.util.CreditCard.KEY_HASH_CVV
+import cesar1287.com.github.desafiopicpay.core.util.CreditCard.KEY_HASH_EXPIRY_DATE
+import cesar1287.com.github.desafiopicpay.core.util.CreditCard.KEY_HASH_NAME
+import cesar1287.com.github.desafiopicpay.features.creditCard.viewmodel.CreditCardViewModel
 import kotlinx.android.synthetic.main.activity_credit_card.*
 
 class CreditCardActivity : AppCompatActivity() {
+
+    private var creditCardViewModel: CreditCardViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_credit_card)
 
+        setupObservables()
         setupToolbar()
+    }
+
+    private fun setupObservables() {
+        creditCardViewModel = ViewModelProviders.of(this).get(CreditCardViewModel::class.java)
+
+        creditCardViewModel?.getAllCreditCards()
+
+        btCreditCardSave.setOnClickListener {
+            val creditCardHashMap: HashMap<String, String> = hashMapOf(
+                KEY_HASH_CARD_NUMBER to tieCreditCardNumber.text.toString(),
+                KEY_HASH_NAME to tieCreditCardName.text.toString(),
+                KEY_HASH_EXPIRY_DATE to tieCreditCardExpire.text.toString(),
+                KEY_HASH_CVV to tieCreditCardCvv.text.toString()
+            )
+
+            creditCardViewModel?.save(creditCardHashMap)
+        }
     }
 
     private fun setupToolbar() {
--- app/src/main/java/cesar1287/com/github/desafiopicpay/features/creditCard/viewmodel/CreditCardViewModel.kt
+++ app/src/main/java/cesar1287/com/github/desafiopicpay/features/creditCard/viewmodel/CreditCardViewModel.kt
@@ -1,2 +1,65 @@
 package cesar1287.com.github.desafiopicpay.features.creditCard.viewmodel
 
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import cesar1287.com.github.desafiopicpay.core.database.PicPayDatabase
+import cesar1287.com.github.desafiopicpay.core.model.CreditCard
+import cesar1287.com.github.desafiopicpay.core.repository.creditCard.CreditCardRepository
+import cesar1287.com.github.desafiopicpay.core.util.CreditCard.KEY_HASH_CARD_NUMBER
+import cesar1287.com.github.desafiopicpay.core.util.CreditCard.KEY_HASH_CVV
+import cesar1287.com.github.desafiopicpay.core.util.CreditCard.KEY_HASH_EXPIRY_DATE
+import cesar1287.com.github.desafiopicpay.core.util.CreditCard.KEY_HASH_NAME
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+
+class CreditCardViewModel(application: Application) : AndroidViewModel(application) {
+
+    private var parentJob = Job()
+
+    private val coroutineContext: CoroutineContext
+        get() = parentJob + Dispatchers.Main
+
+    private val scope = CoroutineScope(coroutineContext)
+
+    private val repository: CreditCardRepository
+
+    var allCreditCards: MutableLiveData<List<CreditCard>> = MutableLiveData()
+
+    init {
+        val creditCardDao = PicPayDatabase.getDatabase(application).creditCardDao()
+
+        repository = CreditCardRepository(creditCardDao)
+    }
+
+    fun save(creditCardHashMap: HashMap<String, String>) {
+        val creditCard = CreditCard(
+            name = creditCardHashMap[KEY_HASH_NAME],
+            cardNumber = creditCardHashMap[KEY_HASH_CARD_NUMBER],
+            cvv = creditCardHashMap[KEY_HASH_CVV],
+            expiryDate = creditCardHashMap[KEY_HASH_EXPIRY_DATE]
+        )
+
+        insert(creditCard)
+    }
+
+    private fun insert(creditCard: CreditCard) = scope.launch(Dispatchers.IO) {
+        repository.insert(creditCard)
+    }
+
+    fun getAllCreditCards() {
+        scope.launch(Dispatchers.IO) {
+            allCreditCards.postValue(repository.getAllCreditCards())
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        parentJob.cancel()
+    }
+}
