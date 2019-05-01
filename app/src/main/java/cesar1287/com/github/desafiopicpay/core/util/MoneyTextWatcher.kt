09bf6243ee26616ad92338a5ee9db510d0867cb6
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2019-05-01 19:10:22 -0300

    minor adjust in MoneyTextWatcher

--- app/src/main/java/cesar1287/com/github/desafiopicpay/core/util/MoneyTextWatcher.kt
+++ app/src/main/java/cesar1287/com/github/desafiopicpay/core/util/MoneyTextWatcher.kt
@@ -12,13 +12,9 @@ import java.util.*
 
 class MoneyTextWatcher(private var editTextWeakReference: WeakReference<EditText>) : TextWatcher {
 
-    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
-        //todo
-    }
+    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
 
-    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-        //todo
-    }
+    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
 
     override fun afterTextChanged(editable: Editable) {
         val editText = editTextWeakReference.get() ?: return
